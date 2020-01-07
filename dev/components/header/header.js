cba1113d817a948c106f6e028239b9c61c9ee304
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2020-01-08 00:31:44 +0300

    About section created

--- dev/components/header/header.js
+++ dev/components/header/header.js
@@ -1,13 +1,11 @@
-const sections = [$('.hero'), $('.why')];
+const sections = [$('.hero'), $('.why'), $('.about'), $('.faq')];
 
 const setLinks = () => {
   sections.map(el => {
-    let isSetted = false;
     const topOffset = el.offset().top;
-    if (!isSetted && topOffset <= window.scrollY + $('.header').innerHeight() + 30) {
+    if (topOffset <= window.scrollY + $('.header').innerHeight() + 30) {
       $('.header__link--current').removeClass('header__link--current');
       $(`.header__link[data-scroll=${el[0].classList[0]}]`).addClass('header__link--current');
-      isSetted = true;
     }
   });
 };
